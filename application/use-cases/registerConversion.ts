6adb747d924e6186a006b44aa3ee5f6ae7889fb0
Oscar Francisco Reyes Guevara <oscar.reyes@est.ulsa.edu.ni>
2026-04-25 00:37:55 -0600

    mi primer use case, calidad

---
+++ application/use-cases/registerConversion.ts
@@ -0,0 +1,82 @@
+import { Moneda } from "../../src/domain/entities/moneda";
+import { Conversion } from "../../src/domain/entities/conversion";
+
+// Dependencias
+
+export interface ExchangeRateService {
+    obtenerTasa(origen: Moneda, destino: Moneda): Promise<number>;
+}
+
+export interface ConversionRepository {
+    guardar(conversion: Conversion): Promise<void>;
+    buscarPorId(id:string): Promise<Conversion | null>;
+}
+
+// Caso de uso
+
+export class RegisterConversion {
+
+    constructor (
+        private exchangeRateService: ExchangeRateService,
+        private conversionRepository: ConversionRepository
+    ) {}
+
+    async ejecutar (
+        id: string,
+        monedaOrigen: Moneda,
+        monedaDestino: Moneda,
+        montoOriginal: number,
+        fecha?: Date,
+    ): Promise<Conversion> {
+
+        // Validaciones
+
+        if (!id || id.trim() === "") {
+            throw new Error("Opa, id vacío");
+        }
+
+        if (montoOriginal <= 0 ){
+            throw new Error("El monto original no puede ser cero");
+        }  
+
+        if (!monedaOrigen || !monedaDestino){
+            throw new Error("Necesito las dos monedas para hacer la conversion");
+        }
+
+        // Reglas de negocio
+
+        if (monedaDestino.obtenerCodigo() === monedaOrigen.obtenerCodigo()){
+            throw new Error("No se puede convertir a la misma moneda");
+        }
+
+        const id_exists = await this.conversionRepository.buscarPorId(id)
+
+        if (id_exists) {
+            throw new Error(`Conversion con id ${id} previamente existente`);
+        }
+
+        const tasa = await this.exchangeRateService.obtenerTasa(monedaOrigen, monedaDestino);
+
+        const montoConvertido = montoOriginal * tasa;
+
+        const conversion = new Conversion(
+            id, 
+            monedaOrigen, 
+            monedaDestino, 
+            montoOriginal, 
+            montoConvertido, 
+            tasa, 
+            fecha || new Date ()
+        );
+
+        if (!conversion.esValida()){
+            throw new Error('Negativo, esto es ilegalisimo')
+        }
+
+        await this.conversionRepository.guardar(conversion)
+
+        return conversion
+
+    }
+
+}
